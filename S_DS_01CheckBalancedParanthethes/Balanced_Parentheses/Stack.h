ef868350fd0fb0615c81f1ed814ad600aa2f4549
arafa arafa <arafa.arafa.prog@gmail.com>
2023-03-28 01:06:17 +0200

    Parentheses_is_Balanced

---
+++ S_DS_01CheckBalancedParanthethes/Balanced_Parentheses/Stack.h
@@ -0,0 +1,26 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include "std_types.h"
+
+#define STACK_SIZE 10
+typedef struct {
+    uint8_t elements[STACK_SIZE];
+    int8_t top;
+
+} ST_stack_t;
+
+void createEmptyStack(ST_stack_t *stack);
+
+int8_t push(ST_stack_t *stack, uint8_t data);
+
+int8_t pop(ST_stack_t *stack, uint8_t *data);
+
+int8_t printStack(ST_stack_t *stack);
+
+int8_t getStackTop(ST_stack_t *stack, uint8_t *topData);
+
+int8_t isEmpty(ST_stack_t *stack);
+int8_t isFull(ST_stack_t *stack);
+
+#endif // STACK_H
---
+++ S_DS_01CheckBalancedParanthethes/Balanced_Parentheses/balancedparantheths.h
@@ -0,0 +1,9 @@
+#ifndef BALANCED_PARANTHETHES_H
+#define BALANCED_PARANTHETHES_H
+
+#include "Stack.h"
+#include "std_types.h"
+
+int8_t isBalancedParanthethes(uint8_t *expression);
+
+#endif // BALANCED_PARANTHETHES_H
