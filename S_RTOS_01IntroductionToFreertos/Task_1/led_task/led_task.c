a912ad6cbac2614c7f4f98ee0638f5f5f44240db
arafaarafa <55886382+arafaarafa@users.noreply.github.com>
2023-07-10 06:04:40 +0300

    delete magic numbers

--- S_RTOS_01IntroductionToFreertos/Task_1/led_task/led_task.c
+++ S_RTOS_01IntroductionToFreertos/Task_1/led_task/led_task.c
@@ -7,9 +7,9 @@ void led_task(void *pvParameters){
 	for( ; ; ){
 		
 				GPIO_write(PORT_0, PIN1, PIN_IS_HIGH);
-				vTaskDelay(1000);
+				vTaskDelay(LED_DELAY_TASK_1_SEC);
 				GPIO_write(PORT_0, PIN1, PIN_IS_LOW);
-				vTaskDelay(1000);
+				vTaskDelay(LED_DELAY_TASK_1_SEC);
 	
 	}
 }
